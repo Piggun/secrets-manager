4d3cabc45c993a013b19e8d03de3e2633a31ca1b
hhcindyhh <mc.sanchezga@gmail.com>
2024-02-08 15:13:42 +0000

    Add function create secret

--- src/app.py
+++ src/app.py
@@ -1,8 +1,24 @@
+import boto3
+client= boto3.client('secretsmanager')
+
+def create_secret(secret_name,user_id,password):
+    response = client.create_secret(
+        Name= secret_name,
+        SecretString= f'{{"user_id":"{user_id}", "password" :"{password}"}}'
+    )
+ 
+
+#create_secret("examplesecret7","user_id7","password7")
+
 def main():
     while True:
         response=input('Please specify [e]ntry, [r]etrieval, [d]eletion, [l]isting or e[x]it\n')
         if response== 'e':
-            print(response)
+            secret_identifier=input('Secret identifier:')
+            user_id=input('UserId:')
+            password=input('Password:')
+            create_secret(secret_identifier,user_id,password)
+            print('Secret has been saved')
         elif response =='r':
             print('retrieve')
         elif response == 'd':
--- src/password_manager.py
+++ src/password_manager.py
@@ -8,32 +8,36 @@ bucket_name="my-boto-bucket-1707303588"
 #s3.upload_file('../file1.txt', bucket_name, 'boto/file1.txt')
 #s3.upload_file('../file2.txt', bucket_name, 'boto/file2.txt')
 
-object_read_list=s3.list_objects_v2(Bucket=bucket_name)
-readable_list=[]
-for file in object_read_list['Contents']:
-    readable_list.append(file["Key"])
-pprint(readable_list)
+#object_read_list=s3.list_objects_v2(Bucket=bucket_name)
+#readable_list=[]
+#if 'Contents' in object_read_list:
+    #for file in object_read_list['Contents']:
+     #   readable_list.append(file["Key"])
+#pprint(readable_list)
 
-response= s3.get_object(Bucket=bucket_name, Key='boto/file1.txt')
-response_body_file1=response['Body'].read().decode('utf-8')
-pprint(response_body_file1)
+#response= s3.get_object(Bucket=bucket_name, Key='boto/file1.txt')
+#response_body_file1=response['Body'].read().decode('utf-8')
+#pprint(response_body_file1)
 
 
-delete_response=s3.delete_objects(
-    Bucket=bucket_name,
-    Delete={
-        'Objects': [
-            {
-                'Key': 'boto/file1.txt' 
-            },
-            {
-                'Key': 'boto/file2.txt'
-            },
-        ],
+#delete_response=s3.delete_objects(
+   # Bucket=bucket_name,
+   # Delete={
+       # 'Objects': [
+        #    {
+             #   'Key': 'boto/file1.txt' 
+           # },
+           # {
+            #    'Key': 'boto/file2.txt'
+           # },
+        #],
 
-    },
-)
-pprint(delete_response)
+    #},
+#)
+#pprint(delete_response)
 
 delete_bucket_response=s3.delete_bucket(Bucket=bucket_name)
-pprint(delete_bucket_response)
+#pprint(delete_bucket_response)
+
+deleted_bucket=s3.list_buckets()
+pprint(deleted_bucket)
